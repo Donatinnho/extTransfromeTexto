6dafb05e0e1aa092341681630b2e2af8d5d4235b
donatinnho <lucas.donato@aluno.senai.br>
2025-10-22 11:07:47 -0300

    Lógica base do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+window.document.addEventListener( "DOMContentLoaded", function (){
+    document.getElementById("btnMaiuscula").addEventListener("click", function(){
+        let inputText = document.querySelector("#entrada").value
+        document.querySelector("#resultado").innerHTML = inputText.toUpperCase()
+    })
+})
+
+window.document.addEventListener( "DOMContentLoaded", function (){
+    document.getElementById("btnMinuscula").addEventListener("click", function(){
+        let inputText = document.querySelector("#entrada").value
+        document.querySelector("#resultado").innerHTML = inputText.toLowerCase()
+    })
+})
