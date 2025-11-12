a54b295e4d3e0eeccbc91020853b9db4816b48b6
donatinnho <lucas.donato@aluno.senai.br>
2025-11-12 08:50:18 -0300

    Atualização de imagens

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-window.document.addEventListener( "DOMContentLoaded", function (){
-    document.getElementById("btnMaiuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#entrada").value
-        document.querySelector("#resultado").innerHTML = inputText.toUpperCase()
-    })
-})
+window.document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#entrada").value;
+      document.querySelector("#resultado").innerHTML = inputText.toUpperCase();
+    });
+});
 
-window.document.addEventListener( "DOMContentLoaded", function (){
-    document.getElementById("btnMinuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#entrada").value
-        document.querySelector("#resultado").innerHTML = inputText.toLowerCase()
-    })
-})
+window.document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#entrada").value;
+      document.querySelector("#resultado").innerHTML = inputText.toLowerCase();
+    });
+});
